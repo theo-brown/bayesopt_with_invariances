fc948e8b1e6f2f8869c3dcd73e4ad54bbe0a2bb9
Theo Brown <7982453+theo-brown@users.noreply.github.com>
2024-01-11 10:33:08 +0000

    Working draft version; acquisition_functions.jl still needs tidying

--- acquisition_functions.jl
+++ acquisition_functions.jl
@@ -1,4 +1,6 @@
-using AbstractGPs, Optim, Zygote, Random, Distributions, ParameterHandling
+using AbstractGPs
+using Optim, Zygote, ParameterHandling
+using Random, Distributions, QuasiMonteCarlo
 
 """
     mvr(posterior_gp, x)
@@ -17,7 +19,8 @@ function mvr(posterior_gp::AbstractGPs.AbstractGP, x::Vector{Float64})::Float64
     x_vec = [x]
 
     # Compute the variance of the GP posterior at x
-    σ2 = var(posterior_gp(x_vec))
+    # Add jitter to improve stability
+    σ2 = var(posterior_gp(x_vec, 1e-6))
 
     # var is a Vector{Float64}, but we only want a single Float64
     return only(σ2)
@@ -40,11 +43,12 @@ function ucb(posterior_gp::AbstractGPs.AbstractGP, x::Vector{Float64})::Float64
     x_vec = [x]
 
     # Compute the mean and variance of the GP posterior at x
-    σ2 = only(var(posterior_gp(x_vec)))
-    μ = only(mean(posterior_gp(x_vec)))
+    finite_gp = posterior_gp(x_vec, 1e-6)
+    σ2 = only(var(finite_gp))
+    μ = only(mean(finite_gp))
 
     if σ2 < 1e-6
-        # If the variance is very small, return the mean. This prevents numerical underflow.
+        # If the variance is very small, return the mean. This prevents numerical underflow in the sqrt.
         return μ
     end
 
@@ -56,28 +60,73 @@ end
 
 Given the GP model, maximise the given acquisition function using multi-start optimisation
 """
+# function maximise_acqf(posterior_gp::AbstractGPs.AbstractGP, acqf::Function, bounds::Vector{Tuple{Float64,Float64}}, n_restarts::Int)
+
+
+#     lower_bounds = [b[1] for b in bounds]
+#     upper_bounds = [b[2] for b in bounds]
+
+#     # Multistart optimisation
+#     start_points = QuasiMonteCarlo.sample(n_restarts, lower_bounds, upper_bounds, LatinHypercubeSample())
+#     # Threads.@threads for k in 1:n_restarts
+#     for k in 1:n_restarts
+#         # Enforce bounds on elements of x0
+#         x0 = [
+#             bounded(start_points[:, k][i], lower_bounds[i], upper_bounds[i])
+#             for i in eachindex(lower_bounds)
+#         ]
+#         # "_transformed" is values transformed into an unbounded space, "_untransformed" is the values in the original bounded space
+#         x0_transformed, untransform = value_flatten(x0)
+
+
+#         function objective(x_untransformed)
+#             return -acqf(posterior_gp, x_untransformed)
+#         end
+
+#         # Maximise the acquisition function by minimising its negative
+#         result = optimize(
+#             objective ∘ untransform,
+#             # x_transformed -> only(Zygote.gradient(objective ∘ untransform, x_transformed)), # Permutation invariant GP is not differentiable
+#             x0_transformed,
+#             inplace=false,
+#             BFGS(
+#                 alphaguess=Optim.LineSearches.InitialStatic(scaled=true),
+#                 linesearch=Optim.LineSearches.BackTracking(),
+#             ),
+#         )
+
+#         candidate_xs[k] = untransform(result.minimizer)
+#         candidate_ys[k] = result.minimum
+#     end
+
+#     # println("candidate_xs = ", candidate_xs)
+#     # println("candidate_ys = ", candidate_ys)
+
+#     # Return the best x
+#     _, i = findmax(candidate_ys)
+#     return candidate_xs[i]
+# end
 function maximise_acqf(posterior_gp::AbstractGPs.AbstractGP, acqf::Function, bounds::Vector{Tuple{Float64,Float64}}, n_restarts::Int)
     candidate_x = nothing
     candidate_y = nothing
 
     # Multistart optimisation
+    lower_bounds = [b[1] for b in bounds]
+    upper_bounds = [b[2] for b in bounds]
+    start_points = [
+        bounded.(x0, lower_bounds, upper_bounds)
+        for x0 in eachcol(QuasiMonteCarlo.sample(n_restarts, lower_bounds, upper_bounds, LatinHypercubeSample()))
+    ]
+
     for k in 1:n_restarts
-        # Generate a random start point within the bounds
-        x0 = [
-            bounded(
-                rand(Uniform(lower, upper)),
-                lower,
-                upper
-            )
-            for (lower, upper) in bounds
-        ]
-        x0_transformed, untransform = value_flatten(x0)
+        x0_transformed, untransform = value_flatten(start_points[k])
 
         function objective(x_untransformed)
             return -acqf(posterior_gp, x_untransformed)
         end
 
         # Maximise the acquisition function by minimising its negative
+        # TODO: This often fails due to Cholesky / not p.d.
         result = optimize(
             objective ∘ untransform,
             # x_transformed -> only(Zygote.gradient(objective ∘ untransform, x_transformed)),
