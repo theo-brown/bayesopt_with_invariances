c764b07ff8e1718416d7e776fb4bf74fdd43910b
Theo Brown <7982453+theo-brown@users.noreply.github.com>
2024-02-06 14:02:49 +0000

    Add option for disabling hyperparameter learning

--- src/bayesopt.jl
+++ src/bayesopt.jl
@@ -3,18 +3,22 @@ include("acquisition.jl")
 include("gp_utils.jl")
 
 
-function run_bayesopt(f, input_bounds, n_steps, gp_builder, acquisition_function)
+function run_bayesopt(f, input_bounds, n_steps, gp_builder, acquisition_function; θ=nothing)
     # Create the output arrays
     d = length(input_bounds)
     observed_x = Matrix{Float64}(undef, n_steps, d)
     observed_y = Vector{Float64}(undef, n_steps)
 
     # Initial GP hyperparameters
-    θ_0 = (
-        σ_f=1.0,
-        l=[1.0 for _ in 1:d],
-        σ_n=0.1
-    )
+    if isnothing(θ)
+        θ_0 = (
+            σ_f=1.0,
+            l=[1.0 for _ in 1:d],
+            σ_n=0.1
+        )
+    else
+        θ_0 = θ
+    end
 
     # Initial sample
     observed_x[1, :] = [
@@ -30,7 +34,7 @@ function run_bayesopt(f, input_bounds, n_steps, gp_builder, acquisition_function
             eachrow(observed_x[1:i, :]), # Use eachrow to turn into an AbstractVector, which is required by AbstractGPs
             observed_y[1:i],
             θ_0;
-            optimise_hyperparameters=true
+            optimise_hyperparameters=isnothing(θ) # If θ is provided, don't optimise the hyperparameters
         )
 
         # Generate the next observation
