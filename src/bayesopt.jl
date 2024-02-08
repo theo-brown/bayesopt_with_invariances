a86bfc5f61f516a7370d282e71c599841e54f056
Theo Brown <7982453+theo-brown@users.noreply.github.com>
2024-02-08 15:51:44 +0000

    Tidy optimise_hyperparameters option

--- src/bayesopt.jl
+++ src/bayesopt.jl
@@ -3,22 +3,19 @@ include("acquisition.jl")
 include("gp_utils.jl")
 
 
-function run_bayesopt(f, input_bounds, n_steps, gp_builder, acquisition_function; θ=nothing)
+function run_bayesopt(
+    f::Function,
+    input_bounds::Vector{Tuple{Float64,Float64}},
+    n_steps::Int,
+    gp_builder::Function,
+    acquisition_function::Function,
+    θ_0::NamedTuple;
+    optimise_hyperparameters::bool=true,
+)
     # Create the output arrays
     d = length(input_bounds)
     observed_x = Matrix{Float64}(undef, n_steps, d)
     observed_y = Vector{Float64}(undef, n_steps)
-
-    # Initial GP hyperparameters
-    if isnothing(θ)
-        θ_0 = (
-            σ_f=1.0,
-            l=[1.0 for _ in 1:d],
-            σ_n=0.1
-        )
-    else
-        θ_0 = θ
-    end
 
     # Initial sample
     observed_x[1, :] = [
@@ -31,10 +28,10 @@ function run_bayesopt(f, input_bounds, n_steps, gp_builder, acquisition_function
         # Update the GP
         gp = get_posterior_gp(
             gp_builder,
-            eachrow(observed_x[1:i, :]), # Use eachrow to turn into an AbstractVector, which is required by AbstractGPs
+            eachrow(observed_x[1:i, :]), # Use eachrow to turn Matrix into an AbstractVector, which is required by AbstractGPs
             observed_y[1:i],
             θ_0;
-            optimise_hyperparameters=isnothing(θ) # If θ is provided, don't optimise the hyperparameters
+            optimise_hyperparameters=optimise_hyperparameters
         )
 
         # Generate the next observation
