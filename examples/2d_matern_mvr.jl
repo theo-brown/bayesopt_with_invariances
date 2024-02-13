f5b61cd67d36f09d61561c3c3bf6e0e38b67c4bb
Theo Brown <7982453+theo-brown@users.noreply.github.com>
2024-02-13 23:08:28 +0000

    Fix bug in misnamed datasets

--- examples/2d_matern_mvr.jl
+++ examples/2d_matern_mvr.jl
@@ -6,7 +6,9 @@ using HDF5
 include("../src/gp_utils.jl")
 include("../src/objective_functions/kernel_objective_function.jl")
 include("../src/bayesopt.jl")
-include("../src/symmetrisation.jl")
+include("../src/invariant_kernels.jl")
+include("../src/groups/permutation_group.jl")
+
 
 ################
 # 0. Arguments #
@@ -23,15 +25,11 @@ s = ArgParseSettings()
 
     "--n_iterations"
     arg_type = Int
-    default = 64
+    default = 10
 
     "--n_repeats"
     arg_type = Int
-    default = 10
-
-    "--beta"
-    arg_type = Float64
-    default = 2.0
+    default = 2
 
     "--latent_function_seed"
     arg_type = Int
@@ -67,7 +65,7 @@ end
 
 # Generate a latent function by evaluating a symmetric Matern-5/2 GP at a grid of points
 # We use a fixed seed to get the same function every time
-latent_function_n_points = 64
+latent_function_n_points = 128
 f = build_latent_function(
     build_2d_invariant_matern52_gp,
     θ,
@@ -128,7 +126,6 @@ savefig(joinpath(args["output_dir"], "figures", "target_function.pdf"))
 # 2. Bayesian optimisation #
 ############################
 output_file = joinpath(args["output_dir"], "results.h5")
-acquisition_function = mvr
 
 h5open(output_file, "w") do file
     # Save metadata
@@ -160,15 +157,51 @@ h5open(output_file, "w") do file
                 bounds,
                 args["n_iterations"],
                 gp_builder,
-                acquisition_function,
+                mvr,
                 θ;
                 optimise_hyperparameters=false
             )
+
+            # Generate the reported values 
+            # MVR's reporting rule is max posterior mean 
+            reported_x = Matrix{Float64}(undef, args["n_iterations"], 2)
+            reported_f = Vector{Float64}(undef, args["n_iterations"])
+            for j in 1:args["n_iterations"]
+                # Generate the GP posterior based on the observations to date
+                gp_posterior = get_posterior_gp(
+                    gp_builder,
+                    eachrow(observed_x[1:j, :]), # Convert from Matrix to Vector
+                    observed_y[1:j],
+                    θ;
+                    optimise_hyperparameters=false
+                )
+
+                # Function to minimise
+                function target(x)
+                    return -only(mean(gp_posterior([x]))) # Extra [] required to turn into vector of vectors, only() required to turn it back
+                end
+                # Start at our best guess
+                maximiser_guess = observed_x[argmax(observed_y), :]
+                # Bound it using ParameterHandling.jl
+                x0 = [
+                    bounded(maximiser_guess[1], bounds[1][1], bounds[1][2]),
+                    bounded(maximiser_guess[2], bounds[2][1], bounds[2][2]),
+                ]
+                x0_flat, unflatten = value_flatten(x0)
+                # Minimise the target
+                result = optimize(target ∘ unflatten, x0_flat, inplace=false)
+
+                # Store the reported value
+                reported_x[j, :] = unflatten(result.minimizer)
+                reported_f[j] = f(reported_x[j, :])
+            end
 
             # Create a group for this repeat
             repeat_group = create_group(gp_group, string(i))
             write_dataset(repeat_group, "observed_x", observed_x)
             write_dataset(repeat_group, "observed_y", observed_y)
+            write_dataset(repeat_group, "reported_x", reported_x)
+            write_dataset(repeat_group, "reported_f", reported_f)
         end
     end
 end
@@ -189,19 +222,22 @@ h5open(output_file, "r") do file
         simple_regret = zeros(n_repeats, n_iterations)
         for i in 1:n_repeats
             repeat_group = gp_group[string(i)]
-            observed_x = read_dataset(repeat_group, "observed_x")
-            true_y = f([collect(xᵢ) for xᵢ in eachrow(observed_x)]) # We have to do this extra collect because f is defined to only take Vector{Vector{Float64}}, rather than AbstractVector
-            # TODO: could use a different reporting rule; original paper uses max posterior mean
+            reported_x = read_dataset(repeat_group, "reported_x")
+            reported_f = read_dataset(repeat_group, "reported_f")
+
             for j in 1:n_iterations
-                simple_regret[i, j] = y_opt - maximum(true_y[1:j])
+                simple_regret[i, j] = y_opt - reported_f[j]
             end
         end
+
+        mean_regret = vec(mean(simple_regret, dims=1))
+        var_regret = vec(var(simple_regret, dims=1))
 
         # Plot the simple regret with error bars
         plot!(
             1:n_iterations,
-            vec(mean(simple_regret, dims=1)),
-            ribbon=2 * vec(std(simple_regret, dims=1)),
+            mean_regret,
+            ribbon=2 * sqrt.(var_regret),
             fillalpha=0.1,
             label=group_name == "permutation_invariant" ? L"$k_G$" : L"$k$",
             xlabel="Iteration",
@@ -217,87 +253,87 @@ savefig(joinpath(args["output_dir"], "figures", "simple_regret.pdf"))
 #######################
 # 4. GP visualisation #
 #######################
-println("Visualising GP predictions...")
+# println("Visualising GP predictions...")
 
-h5open(output_file, "r") do file
-    n_repeats = attrs(file)["n_repeats"]
-    n_iterations = attrs(file)["n_iterations"]
-    for group_name in ["permutation_invariant", "standard"]
-        gp_group = file[group_name]
+# h5open(output_file, "r") do file
+#     n_repeats = attrs(file)["n_repeats"]
+#     n_iterations = attrs(file)["n_iterations"]
+#     for group_name in ["permutation_invariant", "standard"]
+#         gp_group = file[group_name]
 
-        gp_builder = group_name == "permutation_invariant" ? build_2d_invariant_matern52_gp : build_matern52_gp
+#         gp_builder = group_name == "permutation_invariant" ? build_2d_invariant_matern52_gp : build_matern52_gp
 
-        for i in 1:n_repeats
-            repeat_group = gp_group[string(i)]
-            observed_x = read_dataset(repeat_group, "observed_x")
-            observed_y = read_dataset(repeat_group, "observed_y")
-            for j in 1:n_iterations
-                println("($group_name) repeat [$i / $n_repeats] iteration [$j / $n_iterations]")
+#         for i in 1:n_repeats
+#             repeat_group = gp_group[string(i)]
+#             observed_x = read_dataset(repeat_group, "observed_x")
+#             observed_y = read_dataset(repeat_group, "observed_y")
+#             for j in 1:n_iterations
+#                 println("($group_name) repeat [$i / $n_repeats] iteration [$j / $n_iterations]")
 
-                x = eachrow(observed_x[1:j, :])
-                y = observed_y[1:j]
+#                 x = eachrow(observed_x[1:j, :])
+#                 y = observed_y[1:j]
 
-                # Fit a GP to the observed data
-                gp = get_posterior_gp(gp_builder, x, y, θ; optimise_hyperparameters=false)
+#                 # Fit a GP to the observed data
+#                 gp = get_posterior_gp(gp_builder, x, y, θ; optimise_hyperparameters=false)
 
-                # Evaluate on a grid
-                x_range = range(0.0, 1.0, length=100)
-                x_grid = [[x₁, x₂] for x₁ in x_range, x₂ in x_range]
-                gpx = gp([x_grid[i] for i in eachindex(x_grid)], 1e-6)
-                μ = mean(gpx)
-                σ² = var(gpx)
+#                 # Evaluate on a grid
+#                 x_range = range(0.0, 1.0, length=100)
+#                 x_grid = [[x₁, x₂] for x₁ in x_range, x₂ in x_range]
+#                 gpx = gp([x_grid[i] for i in eachindex(x_grid)], 1e-6)
+#                 μ = mean(gpx)
+#                 σ² = var(gpx)
 
-                function plot_with_observations(z, title)
-                    figure = contourf(
-                        x_range,
-                        x_range,
-                        reshape(z, length(x_range), length(x_range)),
-                        levels=32,
-                        color=:viridis,
-                        cbar=false,
-                    )
-                    scatter!(
-                        [xᵢ[1] for xᵢ in x],
-                        [xᵢ[2] for xᵢ in x],
-                        seriestype=:scatter,
-                        color=:white,
-                        label="Observations",
-                        markersize=3,
-                    )
-                    scatter!(
-                        [x[end][1]],
-                        [x[end][2]],
-                        marker=:star,
-                        markersize=10,
-                        color=:red,
-                        label="Last observation"
-                    )
-                    plot!(
-                        legend=false,
-                        title=title,
-                        xlabel=L"$x_1$",
-                        ylabel=L"$x_2$",
-                        size=(300, 300),
-                        aspect_ratio=:equal,
-                        xlims=(0, 1),
-                        ylims=(0, 1),
-                    )
-                    return figure
-                end
+#                 function plot_with_observations(z, title)
+#                     figure = contourf(
+#                         x_range,
+#                         x_range,
+#                         reshape(z, length(x_range), length(x_range)),
+#                         levels=32,
+#                         color=:viridis,
+#                         cbar=false,
+#                     )
+#                     scatter!(
+#                         [xᵢ[1] for xᵢ in x],
+#                         [xᵢ[2] for xᵢ in x],
+#                         seriestype=:scatter,
+#                         color=:white,
+#                         label="Observations",
+#                         markersize=3,
+#                     )
+#                     scatter!(
+#                         [x[end][1]],
+#                         [x[end][2]],
+#                         marker=:star,
+#                         markersize=10,
+#                         color=:red,
+#                         label="Last observation"
+#                     )
+#                     plot!(
+#                         legend=false,
+#                         title=title,
+#                         xlabel=L"$x_1$",
+#                         ylabel=L"$x_2$",
+#                         size=(300, 300),
+#                         aspect_ratio=:equal,
+#                         xlims=(0, 1),
+#                         ylims=(0, 1),
+#                     )
+#                     return figure
+#                 end
 
-                # Plot the GP and acquisition functions
-                μ_figure = plot_with_observations(μ, "Mean")
-                σ²_figure = plot_with_observations(σ², "Variance")
-                gp_figure = plot(μ_figure, σ²_figure, layout=(1, 3), size=(900, 300))
-                savefig(
-                    joinpath(
-                        args["output_dir"],
-                        "figures",
-                        "gp_visualisation",
-                        "$(group_name)_repeat_$(i)_iteration_$(j).png", # Save as PNG because it's easier to flick through
-                    )
-                )
-            end
-        end
-    end
-end
+#                 # Plot the GP and acquisition functions
+#                 μ_figure = plot_with_observations(μ, "Mean")
+#                 σ²_figure = plot_with_observations(σ², "Variance")
+#                 gp_figure = plot(μ_figure, σ²_figure, layout=(1, 2), size=(600, 300))
+#                 savefig(
+#                     joinpath(
+#                         args["output_dir"],
+#                         "figures",
+#                         "gp_visualisation",
+#                         "$(group_name)_repeat_$(i)_iteration_$(j).png", # Save as PNG because it's easier to flick through
+#                     )
+#                 )
+#             end
+#         end
+#     end
+# end
