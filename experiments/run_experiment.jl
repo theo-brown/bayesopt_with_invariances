85af66360f4a32a88eb4c6bc829315d8ea411cb0
Theo Brown <7982453+theo-brown@users.noreply.github.com>
2024-03-07 23:16:14 +0000

    Add support for evaluating multiple different GPs

--- experiments/run_experiment.jl
+++ experiments/run_experiment.jl
@@ -22,12 +22,11 @@ function run_experiment(
     acquisition_function_label::String,
     reporting_function::Function,
     reporting_function_label::String,
-    vanilla_gp_builder::Function,
-    invariant_gp_builder::Function,
-    invariant_gp_label::String,
+    gp_builders::Dict{String, Function},
+    target_gp_builder::Function,
+    target_function_seed::Int,
+    target_function_n_points::Int,
     θ::NamedTuple,
-    latent_function_seed::Int,
-    latent_function_n_points::Int,
 )
     # Setup
     Random.seed!(seed)
@@ -37,11 +36,11 @@ function run_experiment(
     # Define the target function
     println("Generating target function...")
     f = build_latent_function(
-        invariant_gp_builder,
+        target_gp_builder,
         θ,
-        latent_function_n_points,
+        target_function_n_points,
         bounds,
-        latent_function_seed,
+        target_function_seed,
     )
     f_noisy(x) = f(x) + randn() * θ.σ_n
     render(f, bounds; output_filename=joinpath(output_directory, "latent_function"))
@@ -66,8 +65,8 @@ function run_experiment(
         attrs(file)["n_iterations"] = n_iterations
         attrs(file)["n_repeats"] = n_repeats
         attrs(file)["seed"] = seed
-        attrs(file)["latent_function_seed"] = latent_function_seed
-        attrs(file)["latent_function_n_points"] = latent_function_n_points
+        attrs(file)["target_function_seed"] = target_function_seed
+        attrs(file)["target_function_n_points"] = target_function_n_points
         attrs(file)["sigma_n"] = θ.σ_n
         attrs(file)["sigma_f"] = θ.σ_f
         attrs(file)["l"] = θ.l
@@ -76,10 +75,7 @@ function run_experiment(
     end
 
     # Run experiment
-    for (gp_builder, label) in [
-        (vanilla_gp_builder, "Standard"),
-        (invariant_gp_builder, invariant_gp_label),
-    ]
+    for (gp_builder, label) in gp_builders
         println("Running BO with $label kernel...")
 
         h5open(output_file, "r+") do file
