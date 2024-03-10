10e6ccd85b17725ace032b9ac7e12aa1d7849f90
Theo Brown <7982453+theo-brown@users.noreply.github.com>
2024-03-10 11:38:17 +0000

    Add experiment 3

---
+++ experiments/experiment_3_mvr.jl
@@ -0,0 +1,43 @@
+include("run_experiment.jl")
+
+seed = 42
+d = 6
+bounds = [(0.0, 1.0) for _ in 1:d]
+output_directory = "data/experiment_3_mvr_2"
+n_iterations = 512
+n_repeats = 16
+acquisition_function = mvr
+acquisition_function_label = "MVR"
+reporting_function = maximum_observed_posterior_mean
+reporting_function_label = "Maximum observed posterior mean"
+gp_builders = Dict([
+    ("Standard", build_matern52_gp),
+    ("Fully permutation invariant", θ -> build_invariantmatern52_gp(θ, permutation_group(d))),
+    ("2-block permutation invariant", θ -> build_invariantmatern52_gp(θ, blockpermutation_group(d, 2))),
+    ("3-block permutation invariant", θ -> build_invariantmatern52_gp(θ, blockpermutation_group(d, 3))),
+])
+target_gp_builder = gp_builders["Fully permutation invariant"]
+target_function_seed = 1
+target_function_n_points = 512
+θ = (
+    l=0.12,
+    σ_f=1.0,
+    σ_n=0.1,
+)
+
+run_experiment(
+    seed,
+    bounds,
+    output_directory,
+    n_iterations,
+    n_repeats,
+    acquisition_function,
+    acquisition_function_label,
+    reporting_function,
+    reporting_function_label,
+    gp_builders,
+    target_gp_builder,
+    target_function_seed,
+    target_function_n_points,
+    θ,
+)
---
+++ experiments/experiment_3_ucb.jl
@@ -0,0 +1,47 @@
+include("run_experiment.jl")
+
+seed = 42
+d = 6
+bounds = [(0.0, 1.0) for _ in 1:d]
+output_directory = "data/experiment_3_ucb_2"
+n_iterations = 512
+n_repeats = 16
+β = 12.0
+acquisition_function(gp, x) = ucb(gp, x; beta=β)
+acquisition_function_label = "UCB, β=$β"
+reporting_function = latest_point
+reporting_function_label = "Latest point"
+gp_builders = Dict([
+    ("Standard", build_matern52_gp),
+    ("Fully permutation invariant", θ -> build_invariantmatern52_gp(θ, permutation_group(d))),
+    ("2-block permutation invariant", θ -> build_invariantmatern52_gp(θ, blockpermutation_group(d, 2))),
+    ("3-block permutation invariant", θ -> build_invariantmatern52_gp(θ, blockpermutation_group(d, 3))),
+])
+target_gp_builder = gp_builders["Fully permutation invariant"]
+target_function_seed = 1
+target_function_n_points = 512
+θ = (
+    l=0.12,
+    σ_f=1.0,
+    σ_n=0.1,
+)
+
+println("Initial setup completed")
+flush(stdout)
+
+run_experiment(
+    seed,
+    bounds,
+    output_directory,
+    n_iterations,
+    n_repeats,
+    acquisition_function,
+    acquisition_function_label,
+    reporting_function,
+    reporting_function_label,
+    gp_builders,
+    target_gp_builder,
+    target_function_seed,
+    target_function_n_points,
+    θ,
+)
