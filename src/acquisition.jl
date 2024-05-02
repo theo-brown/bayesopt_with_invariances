c9fe9c66481b9d7fd1a34c03143bde39c6e6e3b5
Theo Brown <7982453+theo-brown@users.noreply.github.com>
2024-05-02 15:21:20 +0100

    Add BoTorch-style initialisation heuristic

--- src/acquisition.jl
+++ src/acquisition.jl
@@ -1,12 +1,12 @@
 using AbstractGPs # For general GP functionality
 using Optim, Zygote, ParameterHandling # For optimisation
-using Random, Distributions, QuasiMonteCarlo # For generating spatial samples
+using Random, Distributions, QuasiMonteCarlo, StatsBase # For generating spatial samples
 
 
 """
     mvr(posterior_gp, x)
 
-Compute the uncertainty of a GP posterior at a point x.
+Compute the uncertainty of a GP posterior at point x.
 
 # Arguments
 - `posterior_gp::AbstractGPs.AbstractGP`: A GP posterior.
@@ -15,19 +15,14 @@ Compute the uncertainty of a GP posterior at a point x.
 # Returns
 - `Float64`: The uncertainty of the GP posterior at x.
 """
-function mvr(posterior_gp::AbstractGPs.AbstractGP, x::Vector{Float64})::Float64
-    # Compute the variance of the GP posterior at x
-    # Note that posterior_gp expects a vector of inputs, so we need to wrap x in a vector
-    σ² = var(posterior_gp([x]))
-
-    # var is a Vector{Float64}, but we only want a single Float64
-    return only(σ²)
+function mvr(posterior_gp::AbstractGPs.AbstractGP, x::AbstractVector)
+    return var(posterior_gp(x))
 end
 
 """
     ucb(posterior_gp, x, β)
 
-Compute the upper confidence bound of a GP posterior at a point x.
+Compute the upper confidence bound of a GP posterior at x.
 
 # Arguments
 - `posterior_gp::AbstractGPs.AbstractGP`: A GP posterior.
@@ -36,19 +31,9 @@ Compute the upper confidence bound of a GP posterior at a point x.
 # Returns
 - `Float64`: The upper confidence bound of the GP posterior at x.
 """
-function ucb(posterior_gp::AbstractGPs.AbstractGP, x::Vector{Float64}; beta::Float64=2.0)::Float64
-    # Compute the mean and variance of the GP posterior at x
-    # Note that posterior_gp expects a vector of inputs, so we need to wrap x in a vector
-    finite_gp = posterior_gp([x])
-    σ² = only(var(finite_gp))
-    μ = only(mean(finite_gp))
-
-    if σ² < 1e-6
-        # If the variance is very small, return the mean. This prevents numerical underflow in the sqrt.
-        return μ
-    end
-
-    return μ + beta * sqrt(σ²)
+function ucb(posterior_gp::AbstractGPs.AbstractGP, x::AbstractVector; beta::Float64=2.0)
+    finite_gp = posterior_gp(x)
+    return mean(finite_gp) .+ beta .* sqrt.(var(finite_gp))
 end
 
 
@@ -62,12 +47,15 @@ function maximise_acqf(
     acqf::Function,
     bounds::Vector{Tuple{Float64,Float64}},
     n_restarts::Int;
+    n_initial_samples::Int=1024,
+    eta::Float64=2.0,
     time_limit::Int=150,
     acqf_tolerance::Float64=1e-8,
     acqf_x_tolerance::Float64=1e-8,
 )
     function objective(x)
-        return -acqf(posterior_gp, x)
+        # Optim.jl only supports scalars
+        return -only(acqf(posterior_gp, [x]))
     end
 
     function debug_callback(state::Optim.OptimizationState)
@@ -79,20 +67,35 @@ function maximise_acqf(
     upper_bounds = [b[2] for b in bounds]
     d = length(bounds)
 
-    start_points = Matrix{Float64}(undef, d, n_restarts)
     if n_restarts == 1
         # Select a point uniformly at random
-        start_points .= rand.(Uniform(lower, upper) for (lower, upper) in bounds)
+        start_points = rand.(Uniform(lower, upper) for (lower, upper) in bounds)
     else
-        # Select a batch of points using QMC sampling
+        # This initialisation heuristic is based on the one in BoTorch
+        # 1. Select a large batch of points using QMC sampling
         # Note: LatinHypercubeSample and SobolSample are type unstable at the time of writing
-        start_points .= QuasiMonteCarlo.sample(n_restarts, lower_bounds, upper_bounds, QuasiMonteCarlo.HaltonSample())
+        candidate_start_points = QuasiMonteCarlo.sample(n_initial_samples, lower_bounds, upper_bounds, QuasiMonteCarlo.HaltonSample())
+
+        # 2. Compute the acquisition function at each point
+        candidate_acqf_values = acqf(posterior_gp, ColVecs(candidate_start_points))
+
+        # 3. Sample from a multinomial distribution with probabilities proportional to the acquisition function values
+        zero_mask = candidate_acqf_values .≈ 0
+        μ = mean(candidate_acqf_values)
+        σ = std(candidate_acqf_values)
+        normalised_candidate_acqf_values = (candidate_acqf_values .- μ) ./ σ
+        weights = exp.(eta .* normalised_candidate_acqf_values)
+        weights[zero_mask] .= 0
+        indices = sample(1:n_initial_samples, Weights(weights), n_restarts, replace=false)
+        start_points = candidate_start_points[:, indices]
     end
+
     bounded_start_points = bounded.(start_points, lower_bounds, upper_bounds)
 
     candidate_x = Matrix{Float64}(undef, d, n_restarts)
     candidate_y = Vector{Float64}(undef, n_restarts)
 
+    # TODO: we can definitely batch this, which would be wayy faster
     Threads.@threads for i in 1:n_restarts
         #for i in 1:n_restarts
         x0_transformed, untransform = value_flatten(bounded_start_points[:, i])
