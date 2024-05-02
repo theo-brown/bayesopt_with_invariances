49775d30b9161e668549255bbd30d048c8cda268
Theo Brown <7982453+theo-brown@users.noreply.github.com>
2024-05-02 16:34:11 +0100

    Reuse maximise acqf for reporting in MVR

--- src/reporting.jl
+++ src/reporting.jl
@@ -1,6 +1,7 @@
 using AbstractGPs # For the GP type
 using ParameterHandling, Optim # For optimisation
 
+include("acquisition.jl")
 
 """
     latest_point(posterior_gp, observed_x, observed_y, bounds)
@@ -32,20 +33,9 @@ end
 Return the point in the entire domain that has the highest posterior mean.
 """
 function maximum_posterior_mean(posterior_gp::AbstractGPs.AbstractGP, observed_x::Matrix{Float64}, observed_y::Vector{Float64}, bounds::Vector{Tuple{Float64,Float64}})
-    # Function to minimise
-    function target(x)
-        return -only(mean(posterior_gp([x])))
+    function acqf(posterior_gp::AbstractGPs.AbstractGP, x::AbstractVector)
+        return mean(posterior_gp(x))
     end
 
-    # Start at our best guess
-    maximiser_guess = observed_x[:, argmax(observed_y)]
-    # Bound it using ParameterHandling.jl
-    x0 = [
-        bounded(maximiser_guess[i], bounds[i][1], bounds[i][2])
-        for i in eachindex(bounds)
-    ]
-    x0_flat, unflatten = value_flatten(x0)
-    # Minimise the target
-    result = optimize(target ∘ unflatten, x0_flat)
-    return unflatten(result.minimizer)
+    return maximise_acqf(posterior_gp, acqf, bounds, 10)
 end
