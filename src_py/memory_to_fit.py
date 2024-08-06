e00b2e7b5503c84e72ae6222fea7fb36c4d12f0e
Theo Brown <7982453+theo-brown@users.noreply.github.com>
2024-08-06 07:53:13 +0100

    Add memory benchmark script

---
+++ src_py/memory_to_fit.py
@@ -0,0 +1,78 @@
+import argparse
+
+import h5py
+import torch
+from botorch.fit import fit_gpytorch_mll
+from botorch.models import SingleTaskGP
+from gpytorch.kernels import MaternKernel, ScaleKernel
+from gpytorch.mlls import ExactMarginalLogLikelihood
+from invariant_kernel import InvariantKernel
+from synthetic_objective import create_synthetic_objective
+from transformation_groups import block_permutation_group, permutation_group
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--device", type=str, default="cpu")
+args = parser.parse_args()
+device = torch.device(args.device)
+
+# Options
+n_points_to_fit = 64
+n_seeds = 100
+n_seeds = 100
+n_runs_per_seed = 100
+d = 6
+
+# Create synthetic objective
+f = create_synthetic_objective(
+    d=6,
+    kernel=InvariantKernel(MaternKernel(nu=2.5), permutation_group),
+    seed=0,
+    n_initial_points=512,
+    device=device,
+) 
+
+# Define kernels
+kernels = {
+    "standard": ScaleKernel(MaternKernel(nu=2.5)),
+    "3_block_permutation_augmented": ScaleKernel(MaternKernel(nu=2.5)),
+    "2_block_permutation_augmented": ScaleKernel(MaternKernel(nu=2.5)),
+    "3_block_permutation_invariant": ScaleKernel(InvariantKernel(MaternKernel(nu=2.5), lambda x: block_permutation_group(x, 3))),
+    "2_block_permutation_invariant": ScaleKernel(InvariantKernel(MaternKernel(nu=2.5), lambda x: block_permutation_group(x, 2))),
+    "permutation_invariant": ScaleKernel(InvariantKernel(MaternKernel(nu=2.5), permutation_group)),
+}
+
+# Define benchmark 
+def benchmark_memory(seed, label, kernel):
+    # Reset allocated memory
+    torch.cuda.reset_max_memory_allocated() 
+    
+    # Seed RNG
+    torch.manual_seed(seed)
+    # Generate training data
+    x = torch.rand(n_points_to_fit, d, device=device, dtype=torch.float64)
+    # Augment, if required
+    if "augmented" in label:
+        x_augmented = block_permutation_group(x, int(label[0])).reshape(-1, d)
+        x = torch.cat([x, x_augmented], dim=0)
+    y = (f(x) + 0.01*torch.randn(x.shape[0], device=device)).to(dtype=torch.float64).unsqueeze(-1)
+                
+    # Define the model
+    model = SingleTaskGP(x, y, covar_module=kernel)
+    mll = ExactMarginalLogLikelihood(model.likelihood, model)
+    fit_gpytorch_mll(mll)
+    
+    # Return the peak memory usage
+    return torch.cuda.max_memory_allocated()
+
+
+# Run benchmarks
+for label, kernel in kernels.items():
+    print("Benchmarking kernel", label)
+    max_memory = torch.empty(n_seeds, dtype=torch.float64)
+    for i, seed in enumerate(range(n_seeds)):
+        max_memory[i] = benchmark_memory(seed, label, kernel)
+    
+    with h5py.File("experiments/synthetic/data/memory_benchmark_results.h5", "a") as h5:
+        h5[label] = max_memory.detach().cpu().numpy()
+
+    print("Done.")
